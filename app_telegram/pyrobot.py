cf6027fdd1f10f387c41bef7ae566275d1c84701
lastlibertarian <lastlibertarian@gmail.com>
2022-11-12 13:37:52 +0300

    Revert "start"
    This reverts commit d9de3770890cf5eca3bdc4f413470bb32653e933.

--- app_telegram/pyrobot.py
+++ app_telegram/pyrobot.py
@@ -1,9 +1,2 @@
 from pyrogram import Client
-import configparser
-import os
-from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoTelegram.settings")
-
-
-print(settings.BASE_DIR)
