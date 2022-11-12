d9de3770890cf5eca3bdc4f413470bb32653e933
lastlibertarian <lastlibertarian@gmail.com>
2022-11-12 13:20:27 +0300

    start

--- app_telegram/pyrobot.py
+++ app_telegram/pyrobot.py
@@ -1,2 +1,9 @@
 from pyrogram import Client
+import configparser
+import os
+from django.conf import settings
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoTelegram.settings")
+
+
+print(settings.BASE_DIR)
